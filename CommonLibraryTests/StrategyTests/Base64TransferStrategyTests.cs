130fd883ad979a75da07037515526259e42da9e3
Piotr Rabiniak <pira@conscia.com>
2024-08-11 16:32:57 +0200

    Add some unit tests to CommonLibrary Strategies

---
+++ CommonLibraryTests/StrategyTests/Base64TransferStrategyTests.cs
@@ -0,0 +1,26 @@
+﻿using CommonLibrary.Strategy;
+using FluentAssertions;
+
+namespace CommonLibraryTests.StrategyTests
+{
+    public class Base64TransferStrategyTests
+    {
+        private Base64TransferStrategy _strategy;
+        public Base64TransferStrategyTests()
+        {
+            _strategy = new Base64TransferStrategy();
+        }
+
+        [Theory()]
+        [InlineData("test")]
+        [InlineData("abracadabra")]
+        [InlineData("ąężzźqlł")]
+        public void Encrypt_ShouldBeRepetitive_IsRepetitive(string input)
+        {
+            var result1 = _strategy.Encrypt(input);
+            var result2 = _strategy.Decrypt(result1);
+
+            result2.Should().Be(input);
+        }
+    }
+}
---
+++ CommonLibraryTests/StrategyTests/Rfc2898EncryptStrategyTests.cs
@@ -0,0 +1,28 @@
+﻿using CommonLibrary.Strategy;
+using FluentAssertions;
+
+namespace CommonLibraryTests.StrategyTests
+{
+    public class Rfc2898EncryptStrategyTests
+    {
+        private Rfc2898EncryptStrategy _strategy;
+        public Rfc2898EncryptStrategyTests()
+        {
+            _strategy = new Rfc2898EncryptStrategy();
+        }
+
+        [Theory()]
+        [InlineData("test")]
+        [InlineData("abracadabra")]
+        [InlineData("ąężzźqlł")]
+        public void Encrypt_ShouldBeRepetitive_IsRepetitive(string input)
+        {
+            string empty = string.Empty;
+
+            var result1 = _strategy.Encrypt(input);
+            var result2 = _strategy.Encrypt(input);
+
+            result1.Should().Be(result2);
+        }
+    }
+}
