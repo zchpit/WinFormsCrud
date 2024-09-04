d0f7cd6b65ba1bc4882af05ec1d9829afdd4937b
rabiniak.piotr@gmail.com <rabiniak.piotr@gmail.com>
2024-09-04 11:14:18 +0200

    LoggerManager should write class where exception happend

--- SimpleWebApi/Helpers/LoggerManager.cs
+++ SimpleWebApi/Helpers/LoggerManager.cs
@@ -5,9 +5,9 @@ namespace SimpleWebApi.Helpers
     public class LoggerManager : ILoggerManager
     {
         private static NLog.ILogger logger = LogManager.GetCurrentClassLogger();
-        public void LogDebug(string message) => logger.Debug(message);
-        public void LogError(string message) => logger.Error(message);
-        public void LogInfo(string message) => logger.Info(message);
-        public void LogWarn(string message) => logger.Warn(message);
+        public void LogDebug(string message) => logger.Debug(string.Concat("Logger: ", logger.Name, " Message: ", message));
+        public void LogError(string message) => logger.Error(string.Concat("Logger: ", logger.Name, " Message: ", message));
+        public void LogInfo(string message) => logger.Info(string.Concat("Logger: ", logger.Name, " Message: ", message));
+        public void LogWarn(string message) => logger.Warn(string.Concat("Logger: ", logger.Name, " Message: ", message));
     }
 }
